0f78dc3aeb0dc6c6b389b739cbd9923ffdec874a
SurferClub <harley98.01@hotmail.com>
2023-01-20 10:46:15 -0500

    navbar con estilos

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import TaskForm from './components/TaskForm'
-import TaskList from './components/TaskList'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import Menu from "./components/Navbar";
+import { Container } from "@mui/material";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/task/new' element={<TaskForm/>}></Route>
-      <Route path='/' element={<TaskList/>}></Route>
-    </Routes>
-    
+        <Menu />
+      <Container>
+        <Routes>
+          <Route path="/task/new" element={<TaskForm />}></Route>
+          <Route path="/" element={<TaskList />}></Route>
+        </Routes>
+      </Container>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import {
+  Container,
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+} from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
 
-function Navbar() {
+export default function Navbar() {
+  const navigate = useNavigate();
   return (
-    <div>Navbar</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="transparent">
+        <Container>
+          <Toolbar>
+            <Typography variant='h6' sx={{ flexGrow: 1}}>
+              <Link to="/" style={{textDecoration:"none", color:"#eee"}} >PERN STACK</Link>
+            </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => navigate("/task/new")}
+            >
+              New Task
+            </Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default Navbar
